2f3a7360c1fd8c716e6f6bacc764bba5a3093ce9
janczak <bartlomiej.janczak@gmail.com>
2013-04-05 21:43:09 +0200

    Dodanie definicji grafiki img_15 - dekompozycji przestrzeni punktów za pomocą drzewa vp.

---
+++ documents/image_generation/generate_img_15.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for file_name in $( find . -name img_15.tex )
+do
+  file_name_base=${file_name//.tex/};
+  pdf_file_name=$(eval "echo $file_name_base.pdf");
+  pdf_file_name=${pdf_file_name//\/tex/}
+  png_file_name=$(eval "echo $file_name_base.png");
+  png_file_name=${png_file_name//\/tex/\/img}
+
+  echo "Building" $pdf_file_name "..."
+
+  pdflatex $file_name;
+
+  echo "Building" $pdf_file_name "successfully finished!"
+  echo "Generating" $png_file_name ".."
+
+  convert -density 800 $pdf_file_name -quality 100 $png_file_name
+
+  echo "Generating" $png_file_name "successfully finished!"
+done
+
+echo "Clearing *.aux, *.log, *.pdf files...";
+rm -r *.aux *.log *.pdf
